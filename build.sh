d74ed2e19b89cf35c7c229b10544b6eae6d6922c
marc mckernan <marc.mckernan@gmail.com>
2019-11-02 06:37:10 -0500

    ipdate

--- build.sh
+++ build.sh
@@ -2,37 +2,48 @@
 set -x
 set -e
 
+APPNAME="webapp"
+
 function set_version() {
     os=`uname`
     version_string=`grep app_version app.ini`
     version=${version_string#app_version: }
     if [ "$os" == "Darwin" ] ; then
-        sed -i "" -e s/^app_version.\*/app_version=\"${version}\"/ app/webapp.py
-        sed -i "" -e s/^version.\*/version:\ \"${version}\"/ chart/simpleweb/Chart.yaml
+        sed -i "" -e s/^app_version.\*/app_version=\"${version}\"/ app/${APPNAME}.py
+        sed -i "" -e s/^version.\*/version:\ \"${version}\"/ chart/${APPNAME}/Chart.yaml
     else
-        sed -i -e s/^app_version.\*/app_version=\"${version}\"/ app/webapp.py
-        sed -i -e s/^version.\*/version:\ \"${version}\"/ chart/simpleweb/Chart.yaml
+        sed -i -e s/^app_version.\*/app_version=\"${version}\"/ app/${APPNAME}.py
+        sed -i -e s/^version.\*/version:\ \"${version}\"/ chart/${APPNAME}/Chart.yaml
     fi
 
     image_tag_string=`grep image_tag app.ini`
     image_tag=${image_tag_string#image_tag: }
     if  [ "$os" == "Darwin" ] ; then
-        sed -i "" -e s/^\ \ tag:.\*/\ \ tag:\ ${image_tag}/ chart/simpleweb/values.yaml
+        sed -i "" -e s/^\ \ tag:.\*/\ \ tag:\ ${image_tag}/ chart/${APPNAME}/values.yaml
     else
-        sed -i -e s/^\ \ tag:.\*/\ \ tag:\ ${image_tag}/ chart/simpleweb/values.yaml
+        sed -i -e s/^\ \ tag:.\*/\ \ tag:\ ${image_tag}/ chart/${APPNAME}/values.yaml
     fi
 }
+#
+#
+#
 function build_package() {
     pip3 freeze > requirements.txt
     python3 setup.py bdist_wheel
 }
+#
+#
+#
 function build_image() {
-    docker build --build-arg appversion=${version} -t simpleweb .
-    docker tag simpleweb mmckernan/simpleweb:${image_tag}
-    docker push mmckernan/simpleweb:${image_tag}
+    docker build --build-arg appversion=${version} -t ${APPNAME} .
+    docker tag webapp mmckernan/${APPNAME}:${image_tag}
+    docker push mmckernan/${APPNAME}:${image_tag}
 }
+#
+#
+#
 function build_chart() {
-    helm package chart/simpleweb/    
+    helm package chart/${APPNAME}/
 }
 
 set_version
--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
-docker run -t --rm --name simpleweb -p 5000:5000 \
-           -v /Users/marc/local/python-project/simpleweb/secrets:/secrets \
-           --env CERTFILE=/secrets/localhost.cert.pem \
-           --env KEYFILE=/secrets/localhost.pem simpleweb
+docker run -t --rm --name webapp -p 5000:5000
+
